cdf0016a180af62f101818634ea77f09ad4fadb3
zihanfarah <zihanfarah13@gmail.com>
2023-08-27 22:34:35 +0700

    adding file

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -11,8 +11,8 @@ class LandingController extends Controller
     //
     function index() {
 
-        $user = User::get();
-        dd($user);
-        return view('landing');
+        $user = User::where('id', 7)->first();
+        // dd($user); 
+        return view('landing')->with('user', $user);
     }
 }
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,3 +1,4 @@
 <?php
-echo "Hello, World"
+echo $user->name;
+echo '<br>' . $user->email;
 ?>
